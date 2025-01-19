38bb055a8ca3d7688655cd92dd9b10c3b1d39b37
vector <samcodebello@gmail.com>
2025-01-19 11:49:57 +0100

    added a custom handler to show if the server is still up

--- main.go
+++ main.go
@@ -15,8 +15,15 @@ func main() {
 		Handler: serverHandler,
 	}
 
-	serverHandler.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	serverHandler.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	serverHandler.HandleFunc("/healthz", handlerReadiness)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
